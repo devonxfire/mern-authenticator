fbbdab3e4440567a6eecc461c48bc3878c185b9f
devonmartindale <devonmartindale@gmail.com>
2024-01-19 08:46:46 +0200

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,13 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
---
+++ api/middleware/errorHandler.js
@@ -0,0 +1,6 @@
+// export const errorHandler(err, req, res, next) => {
+
+//     const status = err.statusCode || 500;
+//     const message = err.message;
+
+// }
