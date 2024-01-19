8ed4172c94edef841961acaf52157e8ccc95caa0
devonmartindale <devonmartindale@gmail.com>
2024-01-19 10:30:29 +0200

    complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -4,12 +4,13 @@ dotenv.config();
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-
+import cors from "cors";
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,31 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 uppercase text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          className="bg-slate-700 uppercase text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="mt-5 flex gap-2">
@@ -34,6 +72,7 @@ export default function Signup() {
           <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-700 mt-5">Error!</p>}
     </div>
   );
 }
