2df06e5020459f71cf79525c208ccbf1284be297
devonmartindale <devonmartindale@gmail.com>
2024-01-23 15:19:23 +0200

    complete Home and About pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="px-6 sm:px-2 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold my-8 text-slate-700 text-center sm:text-start">
+        About
+      </h1>
+      <p className="mb-4 text-slate-500 text-sm sm:text-base">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+        voluptates perspiciatis itaque, eius assumenda velit a quidem voluptatum
+        temporibus non consectetur et consequuntur consequatur neque fugit,
+        mollitia amet tempora in vero? Deleniti voluptates autem quisquam sint
+        fugit placeat, libero, quibusdam repellendus perferendis vitae
+        exercitationem explicabo facilis veritatis saepe, tempora dolorem.
+      </p>
+      <p className="mb-4 text-slate-500 text-sm sm:text-base">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+        voluptates perspiciatis itaque, eius assumenda velit a quidem voluptatum
+        temporibus non consectetur et consequuntur consequatur neque fugit,
+        mollitia amet tempora in vero? Deleniti voluptates autem quisquam sint
+        fugit placeat, libero, quibusdam repellendus perferendis vitae
+        exercitationem explicabo facilis veritatis saepe, tempora dolorem.
+      </p>
+      <p className="mb-4 text-slate-500 text-sm sm:text-base">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+        voluptates perspiciatis itaque, eius assumenda velit a quidem voluptatum
+        temporibus non consectetur et consequuntur consequatur neque fugit,
+        mollitia amet tempora in vero? Deleniti voluptates autem quisquam sint
+        fugit placeat, libero, quibusdam repellendus perferendis vitae
+        exercitationem explicabo facilis veritatis saepe, tempora dolorem.
+      </p>
+    </div>
+  );
 }
